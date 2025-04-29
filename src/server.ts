d4f10a02b1827ce8c7b58a9fe09161c82406ff2a
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-04-30 00:26:00 +0530

    fix: validate env var exists before start-up

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import app from "./app";
+import { validateEnv } from "./utils/validateEnv";
 
-const PORT = process.env.PORT || 5000;
+try {
+  validateEnv(["TMDB_API_KEY", "LOOP_FRONTEND_URL"]);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+  const PORT = process.env.PORT || 5000;
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
