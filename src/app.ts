b3bd45e73f39fa6151b7df046a2f1ea6b1dd6b94
Shyam Vishwakarma <shyamvswkrma@gmail.com>
2025-04-30 00:21:36 +0530

    fix: add cors config, endpoints for 404, health

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,40 @@
 import express from "express";
+import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import movieRoutes from "./routes/movieRoutes";
+const frontendUrl = process.env.LOOP_FRONTEND_URL;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: frontendUrl,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Movie API!",
+  });
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    timestamp: new Date().toISOString(),
+    uptime: process.uptime(),
+  });
+});
+
 app.use("/api", movieRoutes);
+
+// Handle 404s for unmatched routes
+app.use((req, res) => {
+  res.status(404).json({ error: "Resource not found" });
+});
 
 export default app;
